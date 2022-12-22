a84ae7b3356a86f7f7baadcac52f6ef6f4a4d7b7
Your Name <you@example.com>
2022-12-22 19:35:37 +0530

    uodated

--- vue routing/app.js
+++ vue routing/app.js
@@ -4,38 +4,41 @@ const player = {
     <router-view />
     </div>`,
     props: ['name'],
-    }
-    const test = {
-    template: '<div><h1> Test Runs: {{ runs }} </h1></div>',
-    
+}
+const test = {
+    template: `<div><h1> Test Runs: {{ runs }} </h1> <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+    <a href="http://127.0.0.1:5500/vue_js_learning/vue%20routing/rou.html#/player/priyanka">Welcome!</a>
+    <router-link to="/player/$name ">Users</router-link>
+      </div>`,
+    props: ['name'],
     data() {
-    return { runs: 5000 }
+        return { name:Ayush, runs: 5000 }
     },
-    }
-    const oneDay = {
+}
+const oneDay = {
     template: '<div><h1> Oneday Runs: {{ runs }} </h1></div>',
     data() {
-    return { runs: 10000 }
+        return { runs: 10000 }
     },
-    }
-    const routes = [
+}
+const routes = [
     {
-    path: '/player/:name',
-    component: player,
-    children: [
-    { path: '', component: oneDay },
-    { path: 'test', component: test },
-    { path: 'oneday', component: oneDay },
-    ],
-    props: true,
+        path: '/player/:name',
+        component: player,
+        children: [
+            { path: '', component: oneDay },
+            { path: 'test', component: test },
+            { path: 'oneday', component: oneDay },
+        ],
+        props: true,
     },
     { path: '*', component: test },
-    ]
-    const router = new VueRouter({
+]
+const router = new VueRouter({
     routes,
     base: '/',
-    })
-    const app = new Vue({
+})
+const app = new Vue({
     el: '#app',
     router,
-    })
+})
